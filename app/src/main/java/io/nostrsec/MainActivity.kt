48fb0edb9f3544991f331a9ced5784f588687d3d
milko <ph.watcharapol_st@tni.ac.th>
2024-02-23 23:15:06 +0700

    ActivityMainBinding

--- app/src/main/java/io/nostrsec/MainActivity.kt
+++ app/src/main/java/io/nostrsec/MainActivity.kt
@@ -1,6 +1,7 @@
 package io.nostrsec
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import io.nostrsec.databinding.ActivityMainBinding
 
@@ -12,5 +13,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setHelloWold()
     }
+
+    private fun setHelloWold() {
+        binding.btn1.setOnClickListener {
+            Toast.makeText(applicationContext, "Hey!!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
