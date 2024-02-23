a159f6ac694caf3212f628a46acc8a4b82820b39
milko <ph.watcharapol_st@tni.ac.th>
2024-02-23 23:23:28 +0700

    Count

--- app/src/main/java/io/nostrsec/MainActivity.kt
+++ app/src/main/java/io/nostrsec/MainActivity.kt
@@ -8,18 +8,21 @@ import io.nostrsec.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var count = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.btn1.text = "Count $count"
         setHelloWold()
     }
 
     private fun setHelloWold() {
         binding.btn1.setOnClickListener {
-            Toast.makeText(applicationContext, "Hey!!", Toast.LENGTH_SHORT).show()
+            count++
+            binding.btn1.text = "Count $count"
         }
     }
 
